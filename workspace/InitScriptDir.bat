ac7ac1ec44c5ba96c35a02fe2be6153803088a82
Igor Grešovnik <gresovnik@gmail.com>
2020-12-08 00:44:55 +0100

    InitScriptDir script completed.

--- workspace/InitScriptDir.bat
+++ workspace/InitScriptDir.bat
@@ -16,30 +16,68 @@ set InitialDir=%CD%
 
 cd %ScriptDir%
 
-if not exist "%CheckoutDir%\.git" (
-  if exist "%CheckoutDir% (
+echo.
+echo.
+echo Initializing the scritp directory:
+echo   %ScriptDir%\%CheckoutDir%
+echo   Repository: %RepositoryAddress%
+echo   Branch: %CheckoutBranch%
+
+
+if not exist "%CheckoutDir%\.git\" (
+  echo Repository not cloned yet.
+  if exist "%CheckoutDir%" (
     echo.
-    echo WARNING: Directory already exists: 
+    echo WARNING: Checkout directory already exists: 
 	echo   %CheckoutDir%
-	echo Operation may fail and you may need to remove the following dir.:
+	echo Git operations may fail and you may need to remove the following  dir.:
 	echo   %ScriptDir%\%CheckoutDir%
 	echo.
   )
-  echo Repository not yet checked out, cloning the code...
+  echo Repository not yet cloned, cloning...
   call git clone %RepositoryAddress% %CheckoutDir%
-  call git remote add origin %RepositoryAddress%
   echo.
 )
 
+echo Changing to checkout directory...
 cd %CheckoutDir%
-call git fetch --all
-call git pull --all
 
+echo Adding remote origin...
+call git remote add origin %RepositoryAddress%
+
+echo Fetching remote branches from origin...
+call git fetch origin
+rem echo Pulling remote branches from origin...
+rem call git pull origin
+
+REM echo Before new checkout block...
+REM if defined CheckoutBranch (
+  REM echo Checking out remote %CheckoutBranch% and track...
+  REM call git checkout --track remotes/origin/%CheckoutBranch%
+  REM echo Checking out without -- track parameter, in case already tracked...
+  REM call git checkout remotes/origin/%CheckoutBranch%
+  REM echo pulling %CheckoutBranch%...
+  REM call git pull origin %CheckoutBranch%
+REM )
+
+
+echo Before ORIGINAL checkout block...
 if defined CheckoutBranch (
-  call git checkout --track origin/%CheckoutBranch%
-  call git pull
+  echo Checking out remote %CheckoutBranch% and track...
+  call git checkout --track remotes/origin/%CheckoutBranch%
+  echo Checking out without -- track parameter in case already tracked...
+  call git checkout remotes/origin/%CheckoutBranch%
+  echo pulling %CheckoutBranch%...
+  call git pull origin %CheckoutBranch%
+) else (
+  REM echo Pulling all tracked branches...
+  REM call git pull origin
 )
 
+
+
+:finalize
+echo.
 
 cd %InitialDir%
 
