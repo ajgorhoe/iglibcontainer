88c6282e2a69ae3c905ad879614b54351b654c7f
Igor Grešovnik <gresovnik@gmail.com>
2021-10-14 23:35:29 +0200

    Removed some legacy code in bootstrapping scripts.

--- workspace/BootStrapUpdate.bat
+++ workspace/BootStrapUpdate.bat
@@ -15,34 +15,3 @@ call "%RedirectScript%"
 
 endlocal
 
-REM Legacy code, can be removed afer some time:
-
-
-
-REM :: Performs bootstrapping by calling BootStrapScripting.bat
-REM :: and in addition, it forces updating IGLibScripts.
-
-REM :: BootStrapScripting.bat itself will skip updating IGLibScripts directory
-REM :: if it is already clones, and the curent script makes sure that update is
-REM :: performed, terefore it can be run whenever one would like to pull the
-REM :: eventual repository updates.
-
-REM setlocal
-
-REM :: Reset the error level (by running an always successfull command):
-REM ver > nul
-
-REM :: Call bootstrapping script to define basic directories and strings:
-REM call "%~dp0BootStrapScripting.bat"
-
-REM :: Skip updating if BootStrapScripting indicated that it has been 
-REM :: performed already:
-REM if %BootstrapRepoUpdated% neq 0 goto SkipUpdating
-	REM :: Update IGLibScripts, even if the repo is already cloned:
-	REM :: Old code: "%UpdateRepo%" "%~dp0\SettingsIGLibScriptsBootstrap.bat"
-	REM "%UpdateRepo%" "%BootstrapSettings%"
-REM :SkipUpdating
-
-REM BootStrapScripting
-
-REM endlocal
