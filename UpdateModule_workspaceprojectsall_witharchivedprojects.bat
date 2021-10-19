307e4ac6b890eb45c75e0411651c1aece0eeb634
Igor Grešovnik <gresovnik@gmail.com>
2021-10-19 21:04:25 +0200

    Removed workspaceprojects/ directory from the repository (from now on, it will be cloned via scripts).

--- UpdateModule_workspaceprojectsall_witharchivedprojects.bat
+++ UpdateModule_workspaceprojectsall_witharchivedprojects.bat
@@ -5,8 +5,13 @@ rem that are all contained in a single repository. Then, the script also
 rem restores the projexts inside workspaceprojects/ that are contained in 
 rem their own individual repositories.
 
+rem If the other repository is currently checked out (the bare container)
+rem then remove the workspaceprojects/ first to create a clean clone:
+if exist "%~dp0\workspaceprojects\0readme_workspaceprojects_container.txt" (
+  rd /s /q "%~dp0\workspaceprojects\"
+)
 
-call "%~dp0\UpdateModule_workspaceprojects_container.bat" 
+call "%~dp0\UpdateModule_workspaceprojects_all.bat" 
 
 call "%~dp0\workspaceprojects\UpdateDirectoryModules_WorkspaceProjects.bat"
 
