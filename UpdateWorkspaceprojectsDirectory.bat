34709bb15c67b2157b13d59cff7c78bfabb1c5c3
Igor Grešovnik <gresovnik@gmail.com>
2021-10-19 21:42:24 +0200

    Condolidated scripts for updating workspaceprojects/ .

--- UpdateWorkspaceprojectsDirectory.bat
+++ UpdateWorkspaceprojectsDirectory.bat
@@ -9,7 +9,7 @@ rem If the other repository is currently checked out (with archive projects)
 rem then remove the workspaceprojects/ first to create a clean clone later:
 if exist "%~dp0\workspaceprojects\0readme_workspaceprojects_all.txt" (
   rd /s /q "%~dp0\workspaceprojects\"
-
+)
 
 call "%~dp0\UpdateModule_workspaceprojects_container.bat" 
 
