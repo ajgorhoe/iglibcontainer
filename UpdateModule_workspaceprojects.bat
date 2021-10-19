bca33701970894ed63d784115d78d64b923b0994
Igor Grešovnik <gresovnik@gmail.com>
2021-10-19 21:11:20 +0200

    Updated scripts to correctly update workspaceprojects/ content from two different containers that can be used (one empty container where all contained directories are updated via contained scripts, and the other one, which is populated with a number of archived (historical) projects that are in this case cloned directly into workspaceprojects/).

--- UpdateModule_workspaceprojects.bat
+++ UpdateModule_workspaceprojects.bat
@@ -1,9 +1,14 @@
 
 rem This script restores / updates the workspaceprojects/ directory by 
-rem cloning the directory that also contains the archived historical projects
-rem that are all contained in a single repository. Then, the script also 
-rem restores the projexts inside workspaceprojects/ that are contained in 
-rem their own individual repositories.
+rem that are all contained in a single repository that does not contain 
+rem any projects by itself.
+rem afterwards, the script also restores the projexts inside workspaceprojects/ 
+rem that are contained in their own individual repositories.
+
+rem If the other repository is currently checked out (with archive projects)
+rem then remove the workspaceprojects/ first to create a clean clone later:
+if exist "%~dp0\workspaceprojects\0readme_workspaceprojects_all.txt" (
+  rd /s /q "%~dp0\workspaceprojects\"
 
 
 call "%~dp0\UpdateModule_workspaceprojects_container.bat" 
--- UpdateModule_workspaceprojectsall_witharchivedprojects.bat
+++ UpdateModule_workspaceprojectsall_witharchivedprojects.bat
@@ -1,6 +1,6 @@
 
 rem This script restores / updates the workspaceprojects/ directory by 
-rem cloning the directory that also contains the archived historical projects
+rem cloning the repository that also contains the archived historical projects
 rem that are all contained in a single repository. Then, the script also 
 rem restores the projexts inside workspaceprojects/ that are contained in 
 rem their own individual repositories.
