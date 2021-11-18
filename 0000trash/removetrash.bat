96958ccb00cfbf5c2e5e6773e4ceefc4463d287f
Igor Grešovnik <gresovnik@gmail.com>
2021-11-18 14:34:19 +0100

    Added trash.

---
+++ 0000trash/removetrash.bat
@@ -0,0 +1,18 @@
+
+
+echo off
+
+echo Removing all directories from the trash directory...
+
+rd /s /q trash_contents
+
+md trash_contents
+
+
+REM rd /s /q \users\0000trash\trash_contents
+
+REM md \users\0000trash\trash_contents
+
+
+
+
