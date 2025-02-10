afa2955382c60941b0198726f81b6834ab146c29
Igor Grešovnik <gresovnik@gmail.com>
2025-02-10 23:47:45 +0100

    Changed main branch for IG:ibScripts to main

--- workspace/Settings00testsIGLibScripts.bat
+++ workspace/Settings00testsIGLibScripts.bat
@@ -30,7 +30,7 @@ echo.
 rem Parameters for the update:
 
 set ModuleDirRelative=00tests/IGLibScripts
-set CheckoutBranch=master
+set CheckoutBranch=main
 set RepositoryAddress=https://github.com/ajgorhoe/IGLib.modules.IGLibScripts.git
 set RepositoryAddressSecondary=https://ajgorhoe@bitbucket.org/ajgorhoe/iglib.modules.iglibscripts.git
 set RepositoryAddressLocal=d:\backup_sync\bk_code\git\ig\misc\iglib_modules\IGLibScripts
