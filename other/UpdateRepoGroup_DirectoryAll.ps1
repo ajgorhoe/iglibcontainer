864b607aa7de325e042f4f84ea6586794a32364b
Igor Grešovnik <gresovnik@gmail.com>
2025-03-12 01:07:59 +0100

    Additional update scripts for iglibmodules/ and variants

--- other/UpdateRepoGroup_DirectoryAll.ps1
+++ other/UpdateRepoGroup_DirectoryAll.ps1
@@ -1,6 +1,6 @@
 
 # Clones or updates all repositories contained in this directory.
-Write-Host "`n`nCloning / updating all repositories in this directory ...`n"
+Write-Host "`n`nCloning / updating all repositories in directory other/ ...`n"
 
 # Get the script directory such that relative paths can be resolved:
 $scriptPath = $MyInvocation.MyCommand.Path
@@ -9,11 +9,14 @@ $scriptFilename = [System.IO.Path]::GetFileName($scriptPath)
 
 Write-Host "Script directory: $scriptDir"
 
+Write-Host "`nUpdating basic directories:"
+& $(Join-Path $scriptDir "UpdateReposGroup_DirectoryBasic.ps1")
+
 Write-Host "`nUpdating iglibmodules:"
-& $(Join-Path $scriptDir "UpdateRepo_iglibmodules.ps1")
+& $(Join-Path $scriptDir "UpdateRepo_iglibmodules_CORE.ps1")
 
 Write-Host "`nUpdating learn_sluzba:"
-& $(Join-Path $scriptDir "UpdateRepo_learn_sluzba.ps1")
+& $(Join-Path $scriptDir "UpdateRepo_iglibmodules_LEGACY.ps1")
 
 Write-Host "  ... updating repositoris in the current directory completed.`n`n"
 
---
+++ other/UpdateRepoGroup_DirectoryBasic.ps1
@@ -0,0 +1,19 @@
+
+# Clones or updates all repositories contained in this directory.
+Write-Host "`n`nCloning / updating all repositories in directory other/ ...`n"
+
+# Get the script directory such that relative paths can be resolved:
+$scriptPath = $MyInvocation.MyCommand.Path
+$scriptDir = Split-Path $scriptPath -Parent
+$scriptFilename = [System.IO.Path]::GetFileName($scriptPath)
+
+Write-Host "Script directory: $scriptDir"
+
+Write-Host "`nUpdating iglibmodules:"
+& $(Join-Path $scriptDir "UpdateRepo_iglibmodules.ps1")
+
+Write-Host "`nUpdating learn_sluzba:"
+& $(Join-Path $scriptDir "UpdateRepo_learn_sluzba.ps1")
+
+Write-Host "  ... updating repositoris in the current directory completed.`n`n"
+
