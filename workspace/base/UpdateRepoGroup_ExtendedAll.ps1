f1b8c24cad4a0d6ae317fc85614b1e7b84e3e4aa
Igor Grešovnik <gresovnik@gmail.com>
2025-07-04 12:45:31 +0200

    Corrected paths in workspace/base/UpdateRepoGroup_ExtendedAll.ps1

--- workspace/base/UpdateRepoGroup_ExtendedAll.ps1
+++ workspace/base/UpdateRepoGroup_ExtendedAll.ps1
@@ -19,11 +19,11 @@ Write-Host "Script directory: $scriptDir"
 Write-Host "`nUpdating extended-all IGLibFramework repositories:`n"
 
 Write-Host "`nUpdating workspaceprojects:"
-& $(Join-Path $scriptDir "_scripts/IGLibFramework/UpdateRepo_workspaceprojects.ps1")
+& $(Join-Path $scriptDir "UpdateRepo_workspaceprojects.ps1")
 
 # clone of workspaceproject_all (below) is not included (this is a very large project)
 Write-Host "`nUpdating workspaceprojects_all:"
-& $(Join-Path $scriptDir "_scripts/IGLibFramework/UpdateRepo_workspaceprojects_all.ps1")
+& $(Join-Path $scriptDir "UpdateRepo_workspaceprojects_all.ps1")
 
 
 
