6bf4689517923ad09ba1e84a0b84cd975d102223
Igor Grešovnik <gresovnik@gmail.com>
2025-07-04 10:52:53 +0200

    Replaced trash dir

--- __trash/removetrash.bat
+++ __trash/removetrash.bat
@@ -4,9 +4,12 @@ echo off
 
 echo Removing all directories from the trash directory...
 
+setlocal
+cd %~dp0
 rd /s /q trash_contents
-
 md trash_contents
+md save
+endlocal
 
 
 REM rd /s /q \users\0000trash\trash_contents
