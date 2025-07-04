6c9cecf936ac4279bae400454138f16d18333ac5
Igor Grešovnik <gresovnik@gmail.com>
2025-07-04 11:45:29 +0200

    Update scripts for repository groups in workspace/base/

---
+++ workspace/base/UpdateRepoGroup_Basic.ps1
@@ -0,0 +1,30 @@
+
+# Clones or updates the IGLib Frameworrk basic repositories in iglibmodules.
+Write-Host "`n`nCloning / updating basic IGLib Framework repositories in iglibmodules ...`n"
+
+# Get the script directory such that relative paths can be resolved:
+$scriptPath = $MyInvocation.MyCommand.Path
+$scriptDir = Split-Path $scriptPath -Parent
+$scriptFilename = [System.IO.Path]::GetFileName($scriptPath)
+
+Write-Host "Script directory: $scriptDir"
+
+
+# First, update group of IGLib basic repositories:
+& $(join-path $scriptDir "UpdateRepos_Basic.ps1")
+
+
+Write-Host "`nUpdating basic IGLibFramework repositories:`n"
+
+Write-Host "`nUpdating iglib:"
+& $(Join-Path $scriptDir "UpdateRepo_iglib.ps1")
+
+Write-Host "`nUpdating iglibexternal:"
+& $(Join-Path $scriptDir "UpdateRepo_iglibexternal.ps1")
+
+Write-Host "`nUpdating igsolutions:"
+& $(Join-Path $scriptDir "UpdateRepo_igsolutions.ps1")
+
+
+Write-Host "  ... updating basic IGLib Framework repositories completed.`n`n"
+
---
+++ workspace/base/UpdateRepoGroup_Extended.ps1
@@ -0,0 +1,44 @@
+
+# Clones or updates the IGLib Framework EXTENDED repositories in iglibmodules.
+Write-Host "`n`nCloning / updating EXTENDED IGLib Framework repositories in iglibmodules ...`n"
+
+# Get the script directory such that relative paths can be resolved:
+$scriptPath = $MyInvocation.MyCommand.Path
+$scriptDir = Split-Path $scriptPath -Parent
+$scriptFilename = [System.IO.Path]::GetFileName($scriptPath)
+
+Write-Host "Script directory: $scriptDir"
+
+
+# First, update group of IGLib Framework's basic repositories.
+# This will also update IGLib basic repositories 
+# (script UpdateRepos_Basic.ps1)
+& $(join-path $scriptDir "UpdateReposOld_Basic.ps1")
+
+
+Write-Host "`nUpdating extended IGLibFramework repositories:`n"
+
+Write-Host "`nUpdating iglibapp:"
+& $(Join-Path $scriptDir "UpdateRepo_iglibapp.ps1")
+
+Write-Host "`nUpdating shelldev:"
+& $(Join-Path $scriptDir "UpdateRepo_shelldev.ps1")
+
+Write-Host "`nUpdating data:"
+& $(Join-Path $scriptDir "UpdateRepo_data.ps1")
+
+Write-Host "`nUpdating iglearn:"
+& $(Join-Path $scriptDir "UpdateRepo_iglearn.ps1")
+
+Write-Host "`nUpdating igsandbox:"
+& $(Join-Path $scriptDir "UpdateRepo_igsandbox.ps1")
+
+Write-Host "`nUpdating unittests:"
+& $(Join-Path $scriptDir "UpdateRepo_unittests.ps1")
+
+Write-Host "`nUpdating igtest:"
+& $(Join-Path $scriptDir "UpdateRepo_igtest.ps1")
+
+
+Write-Host "  ... updating extended IGLib Framework repositories complete.`n`n"
+
