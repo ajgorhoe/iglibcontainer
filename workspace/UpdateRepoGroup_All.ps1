989d11c2b1acb9082d35c2edd116de6e9105284f
Igor Grešovnik <gresovnik@gmail.com>
2025-08-01 16:17:25 +0200

    workspace/UpdateRepoGroup_All.ps1

---
+++ workspace/UpdateRepoGroup_All.ps1
@@ -0,0 +1,23 @@
+
+# Clones or updates the basic repositories in ws/workspace.
+
+# Get the script directory such that relative paths can be resolved:
+$scriptPath = $MyInvocation.MyCommand.Path
+$scriptDir = Split-Path $scriptPath -Parent
+$scriptFilename = [System.IO.Path]::GetFileName($scriptPath)
+
+Write-Host "`nCloning/updating EXTENDED WORKSPACE repositories:`n"
+Write-Host "Script directory: $scriptDir"
+
+Write-Host "`nUpdating IGLibScripts:"
+& $(Join-Path $scriptDir "UpdateRepo_IGLibScripts.ps1")
+
+Write-Host "`nUpdating develop_cobik:"
+& $(Join-Path $scriptDir "UpdateRepo_develop_cobik.ps1")
+
+
+# Finally, also update the BASIC workspace repositories:
+& $(Join-Path $scriptDir "UpdateRepoGroup_WorkspaceBasic.ps1")
+
+Write-Host "  ... updating extended workspace repositories completed.`n`n"
+
