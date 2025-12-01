6f7b52298fc2409b8733b330baaf8f5730e3d6d0
Igor Grešovnik <gresovnik@gmail.com>
2025-12-01 23:41:26 +0100

    UpdateRepo_iglibmodules_Recursive.ps1 - added correct checkout branch

--- other/UpdateRepo_iglibmodules_Recursive.ps1
+++ other/UpdateRepo_iglibmodules_Recursive.ps1
@@ -30,7 +30,7 @@ $UpdatingScriptPath = "../workspace/UpdateOrCloneRepository.ps1"
 #    in the same order as that script's parameters:
 
 $global:CurrentRepo_Directory = "iglibmodules_Recursive/"
-$global:CurrentRepo_Ref = "swrepos/GrLib/repoMain"
+$global:CurrentRepo_Ref = "swrepos/RecursiveIGLib/mainRecursiveIGLib"
 $global:CurrentRepo_Address = "https://github.com/ajgorhoe/iglibmodules.git"
 $global:CurrentRepo_Remote = "origin"
 $global:CurrentRepo_AddressSecondary = "https://gitlab.com/ajgorhoe/iglibmodules.git"
